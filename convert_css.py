39b0aa1e9e6ce0f378530a596881ac4340876438
jaybenaim <benaimjacob@gmail.com>
2019-07-17 00:31:14 -0400

    everything complete

--- convert_css.py
+++ convert_css.py
@@ -3,36 +3,28 @@ import webcolors
 
 sheet = cssutils.parseFile('main.css')
 
-def main(): 
-    f= open("new_css.css", "a+") 
-        
-    for rule in sheet: 
-        if rule.type == rule.STYLE_RULE: 
-            # find property 
-            for property in rule.style:
-                if property.name == 'color':
-                    if property.value != 'inherit': 
-                        if property.value[0] != "#": 
-                            property.value = webcolors.CSS3_NAMES_TO_HEX[property.value]
-                    # property.priority = 'IMPORTANT'
-    sheet.add('@import "sheets/main.css";')
+# def main(): 
+#     f= open("newer_css.css", "a+") 
+    
+for rule in sheet: 
+    if rule.type == rule.STYLE_RULE: 
+        # find property 
+        for property in rule.style:
+            if property.name == 'color':
+                if property.value != 'inherit': 
+                    if property.value[0] != "#": 
+                        property.value = webcolors.CSS3_NAMES_TO_HEX[property.value]
+                # property.priority = 'IMPORTANT'
+
 
 # new = cssutils.parseFile(sheet.cssText)
-# # print(sheet.cssText)
+print(sheet.cssText)
 # print(new) 
 
 
-    new = cssutils.parseFile(sheet.cssText)
-    f.write(new)
-    f.close() 
-
-if __name__=="__main__": 
-    main() 
-
-
-# # Write to a new file
-# with open('style_new.css', 'wb') as f:
-#     f.write(sheet.cssText)
+# Write to a new file
+with open('test_css.css', 'wb') as f:
+    f.write(sheet.cssText)
 
 
 
